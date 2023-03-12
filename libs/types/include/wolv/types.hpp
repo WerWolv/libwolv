e3e6e96825a05544507cd06d55b3b00bfade42f0
WerWolv <werwolv98@gmail.com>
2023-03-12 17:47:46 +0100

    types: Fixed 128 bit types not being usable with -Wpedantic

--- libs/types/include/wolv/types.hpp
+++ libs/types/include/wolv/types.hpp
@@ -8,7 +8,7 @@ namespace wolv {
         using u16 = std::uint16_t;
         using u32 = std::uint32_t;
         using u64 = std::uint64_t;
-        using u128 = unsigned __int128;
+        using u128 = __uint128_t;
 
     }
 
@@ -18,7 +18,7 @@ namespace wolv {
         using i16 = std::int16_t;
         using i32 = std::int32_t;
         using i64 = std::int64_t;
-        using i128 = signed __int128;
+        using i128 = __int128_t;
 
     }
 
