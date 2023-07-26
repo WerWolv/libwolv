57010ec4f1ee3f19ae71c7d74688243509eea23d
iTrooz <hey@itrooz.fr>
2023-07-26 23:30:41 +0200

    add test to scope guard

--- tests/utils/source/guards.cpp
+++ tests/utils/source/guards.cpp
@@ -6,13 +6,22 @@ using namespace wolv::util;
 
 TEST_SEQUENCE("Guard_ScopeGuard") {
 
-    // normal test
+    // ensure block is run at the end of the block and not before
     {
         int i = 0;
         SCOPE_GUARD {
             TEST_ASSERT(i == 1);
         };
         i = 1;
+    }
+
+    // ensure block is actually run
+    {
+        int i = 0;
+        SCOPE_GUARD {
+            i = 1;
+        };
+        TEST_ASSERT(i == 1);
     }
 
     // release the guard
