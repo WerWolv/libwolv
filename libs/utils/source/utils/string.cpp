bc29a4f31743f4a05c2c23aff1168fd0b4d359b9
WerWolv <werwolv98@gmail.com>
2023-12-07 22:51:32 +0100

    utils: Remove empty parts when splitting string

--- libs/utils/source/utils/string.cpp
+++ libs/utils/source/utils/string.cpp
@@ -7,23 +7,26 @@ namespace wolv::util {
     std::vector<std::string> splitString(const std::string &string, const std::string &delimiter) {
         if (delimiter.empty()) {
             return { string };
-        } 
-        size_t start = 0, end = 0;
-        std::string token;
-        std::vector<std::string> res;
+        }
 
+        std::vector<std::string> result;
+
+        size_t start = 0, end = 0;
         while ((end = string.find(delimiter, start)) != std::string::npos) {
             size_t size = end - start;
             if (start + size > string.length())
                 break;
 
-            token = string.substr(start, end - start);
+            auto token = string.substr(start, end - start);
             start = end + delimiter.length();
-            res.push_back(token);
+            result.emplace_back(std::move(token));
         }
 
-        res.emplace_back(string.substr(start));
-        return res;
+        result.emplace_back(string.substr(start));
+
+        std::erase_if(result, [](const auto &string) { return string.empty(); });
+
+        return result;
     }
 
     std::string combineStrings(const std::vector<std::string> &strings, const std::string &delimiter) {
