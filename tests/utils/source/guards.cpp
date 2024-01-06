97f56330ccb311c4e852edbad8b7efee1cb8981e
WerWolv <werwolv98@gmail.com>
2024-01-06 15:44:19 +0100

    Fix issues with tests

--- tests/utils/source/guards.cpp
+++ tests/utils/source/guards.cpp
@@ -7,30 +7,32 @@ using namespace wolv::util;
 TEST_SEQUENCE("Guard_ScopeGuard") {
 
     // ensure block is run at the end of the block and not before
+    int i = 0;
     {
-        int i = 0;
         SCOPE_GUARD {
-            TEST_ASSERT(i == 1);
+            i = 2;
         };
         i = 1;
     }
+    TEST_ASSERT(i == 2);
 
     // ensure block is actually run
     {
-        int i = 0;
         SCOPE_GUARD {
-            i = 1;
+            i = 3;
         };
-        TEST_ASSERT(i == 1);
     }
+    TEST_ASSERT(i == 3);
 
     // release the guard
+    bool guardRan = false;
     {
         auto guard = SCOPE_GUARD {
-            TEST_FAIL();
+            guardRan = true;
         };
         guard.release();
     }
+    TEST_ASSERT(!guardRan);
 
     TEST_SUCCESS();
 };
