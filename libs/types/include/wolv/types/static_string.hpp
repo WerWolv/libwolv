7e90fc85edad5c59eb82ce1d2727e1070fe4229b
WerWolv <werwolv98@gmail.com>
2024-01-22 19:13:06 +0100

    types: Fix static string not working correctly

--- libs/types/include/wolv/types/static_string.hpp
+++ libs/types/include/wolv/types/static_string.hpp
@@ -8,10 +8,10 @@ namespace wolv::type {
     template<size_t N>
     struct StaticString {
         constexpr StaticString(const char (&str)[N]) {
-            std::copy_n(str, N, this->value);
+            std::copy_n(str, N, this->value.begin());
         }
 
-        std::array<char, N> value;
+        std::array<char, N> value = {};
     };
 
 }
