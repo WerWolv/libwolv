c0e3557a72b2efdc1dc7c87026ac6b6ac7ea0b53
iTrooz <hey@itrooz.fr>
2024-05-21 20:03:58 +0200

    add FileMove tests

---
+++ tests/io/source/file_unix.cpp
@@ -0,0 +1,26 @@
+// We are still testing wolv::io::File, but in this file we target code coverage for file_unix.cpp in particular
+
+#include <wolv/test/tests.hpp>
+#include <wolv/types.hpp>
+
+#include <wolv/io/file.hpp>
+
+using namespace std::literals::string_literals;
+using namespace wolv::unsigned_integers;
+
+const auto FilePath    = std::fs::current_path() / "file.txt";
+const auto FileContent = "Hello World";
+
+TEST_SEQUENCE("FileMove") {
+    wolv::io::File file(FilePath, wolv::io::File::Mode::Create);
+    TEST_ASSERT(file.isValid());
+
+    wolv::io::File file2(std::move(file));
+    TEST_ASSERT(file2.isValid());
+
+    wolv::io::File file3;
+    file3 = std::move(file2);
+    TEST_ASSERT(file3.isValid());
+
+    TEST_SUCCESS();
+};
