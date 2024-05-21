e6bbae76527b08aa2162f02b56684cf42d2aa0ad
iTrooz <hey@itrooz.fr>
2024-05-21 18:15:41 +0200

    test: add test for CRC reflection (#21)

--- tests/hash/source/crc.cpp
+++ tests/hash/source/crc.cpp
@@ -14,3 +14,14 @@ TEST_SEQUENCE("CRC") {
 
     TEST_SUCCESS();
 };
+
+TEST_SEQUENCE("CRC_Reflect") {
+    wolv::hash::Crc<32> crc32(0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, false, false);
+
+    std::array<wolv::u8, 5> data = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+
+    crc32.process(data);
+    TEST_ASSERT(crc32.getResult() == 493925500);
+
+    TEST_SUCCESS();
+};
