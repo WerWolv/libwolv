fadfefca534d57b0479ee387e13816c682bb9fbf
WerWolv <werwolv98@gmail.com>
2025-01-29 21:28:35 +0100

    types: Added <=> operator for 128 bit types

--- libs/types/include/wolv/types.hpp
+++ libs/types/include/wolv/types.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cstdint>
 
 // Disable builtin uint128 support when building with the MSVC standard library
@@ -5,7 +7,7 @@
 #undef LIBWOLV_BUILTIN_UINT128
 #endif // LIBWOLV_BUILTIN_UINT128
 
-#ifndef LIBWOLV_BUILTIN_UINT128
+#if !defined(LIBWOLV_BUILTIN_UINT128)
 #include "types/uintwide_t.h"
 #endif // LIBWOLV_BUILTIN_UINT128
 
@@ -18,7 +20,7 @@ namespace wolv {
         using u32 = std::uint32_t;
         using u64 = std::uint64_t;
 
-#ifdef LIBWOLV_BUILTIN_UINT128
+#if defined(LIBWOLV_BUILTIN_UINT128)
         using u128 = __uint128_t;
 #else
         using u128 = ::math::wide_integer::uint128_t;
@@ -33,7 +35,7 @@ namespace wolv {
         using i32 = std::int32_t;
         using i64 = std::int64_t;
 
-#ifdef LIBWOLV_BUILTIN_UINT128
+#if defined(LIBWOLV_BUILTIN_UINT128)
         using i128 = __int128_t;
 #else
         using i128 = ::math::wide_integer::int128_t;
@@ -53,3 +55,25 @@ namespace wolv {
     using namespace floating_point_numbers;
 
 }
+
+#if !defined(LIBWOLV_BUILTIN_UINT128)
+
+inline std::strong_ordering operator<=>(wolv::unsigned_integers::u128 const &lhs, wolv::unsigned_integers::u128 const &rhs) {
+    if (lhs == rhs)
+        return std::strong_ordering::equal;
+    if (lhs < rhs)
+        return std::strong_ordering::less;
+
+    return std::strong_ordering::greater;
+}
+
+inline std::strong_ordering operator<=>(wolv::signed_integers::i128 const &lhs, wolv::signed_integers::i128 const &rhs) {
+    if (lhs == rhs)
+        return std::strong_ordering::equal;
+    if (lhs < rhs)
+        return std::strong_ordering::less;
+
+    return std::strong_ordering::greater;
+}
+
+#endif
