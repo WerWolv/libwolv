45523d1868c4da19bfae1d48a4b1fa75bf9940d0
WerWolv <werwolv98@gmail.com>
2025-12-04 17:26:55 +0100

    utils: Add fallback for from_chars

--- libs/utils/include/wolv/utils/charconv.hpp
+++ libs/utils/include/wolv/utils/charconv.hpp
@@ -16,10 +16,27 @@ namespace wolv::util {
     template<std::floating_point T>
     [[nodiscard]] std::optional<T> from_chars(std::string_view string, std::chars_format format = std::chars_format::general) {
         T value;
-        auto [ptr, ec] = std::from_chars(string.data(), string.data() + string.size(), value, format);
-        if (ec != std::errc() || ptr != string.data() + string.size())
-            return std::nullopt;
-        return value;
+        if constexpr (requires { std::from_chars(string.data(), string.data() + string.size(), value, format); }) {
+            auto [ptr, ec] = std::from_chars(string.data(), string.data() + string.size(), value, format);
+            if (ec != std::errc() || ptr != string.data() + string.size())
+                return std::nullopt;
+            return value;
+        } else {
+            // TODO: This sucks but std::from_chars for floating point types is not supported by some clang versions yet
+            // These functions do not handle errors properly and also are locale dependent.
+            // Once std::from_chars for floating point types is supported, recompiling will
+            // automatically switch to the from_chars implementation above.
+            if constexpr (std::same_as<T, float>)
+                return std::strtof(string.data(), nullptr);
+            else if constexpr (std::same_as<T, double>)
+                return std::strtod(string.data(), nullptr);
+            else if constexpr (std::same_as<T, long double>)
+                return std::strtold(string.data(), nullptr);
+            else {
+                static_assert("T must be a floating point type");
+                return std::nullopt;
+            }
+        }
     }
-    
+
 }
